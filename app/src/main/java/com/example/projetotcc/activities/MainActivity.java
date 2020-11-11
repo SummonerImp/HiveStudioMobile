64967f5d3cf889026ca1b28c9d4165552cb7c69f
SummonerImp <csantanajunior2012@gmail.com>
2020-11-11 17:42:01 -0300

    initial commit

--- app/src/main/java/com/example/projetotcc/activities/MainActivity.java
+++ app/src/main/java/com/example/projetotcc/activities/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        getWindow().setNavigationBarColor(getResources().getColor(R.color.brown));
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.bistre));
+        getWindow().setStatusBarColor(getResources().getColor(R.color.bistre));
     }
 }
